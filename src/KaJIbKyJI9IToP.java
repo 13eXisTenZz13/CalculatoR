ef78f658c4241e2e2734307ecbecc3428f0c51c2
13eXisTenZz13 <aipopravka@yandex.ru>
2024-01-15 00:11:12 +0300

    Initial commit

---
+++ src/KaJIbKyJI9IToP.java
@@ -0,0 +1,138 @@
+
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class KaJIbKyJI9IToP {
+
+    static Scanner scanner = new Scanner(System.in);
+    static int number1;
+    static int number2;
+    static char operation;
+    static int result;
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите выражение: ");
+        String userInput = scanner.nextLine();
+
+        char[] under_char = new char[9];
+        for (int i = 0; i < userInput.length(); i++) {
+            under_char[i] = userInput.charAt(i);
+            if (under_char[i] == '+') {
+                operation = '+';
+            }
+            if (under_char[i] == '-') {
+                operation = '-';
+            }
+            if (under_char[i] == '*') {
+                operation = '*';
+            }
+            if (under_char[i] == '/') {
+                operation = '/';
+            }
+        }
+        if (operation == 0) {
+            throw new IllegalArgumentException("СТРОКА ВВЕДЕНА НЕ ПРАВИЛЬНО");
+        }
+
+        String userClear = userInput.replace(" ", "");
+        String[] chasti = userClear.split("[+\\-/*]");
+        String chast1 = chasti[0];
+        String chast2 = chasti[1];
+
+        try{
+            if (chasti[2] != ""){
+                throw new IllegalArgumentException("ВВЕДЕНО НЕ ВЕРНОЕ ВЫРАЖЕНИЕ");
+            }
+        } catch (ArrayIndexOutOfBoundsException exc){
+        }
+        try {
+            number1 = parseInt(chast1);
+            number2 = parseInt(chast2);
+
+            if ((number1 <= 10) & (number2 <= 10) & (number1 > 0) & (number2 > 0)) {
+                result = calculated(number1, number2, operation);
+                System.out.println("_____Результат для арабских цифр_____");
+                System.out.println(number1 + " " + operation + " " + number2 + " = " + result);
+            } else {
+                throw new IllegalArgumentException("ДОЛЖНЫ ИСПОЛЬЗОВАТЬСЯ ЧИСЛА В ДИАПАЗОНЕ ОТ 1 ДО 10");
+            }
+        } catch (NumberFormatException e) {
+
+            number1 = romanToNumber(chast1);
+            number2 = romanToNumber(chast2);
+
+            if ((number1 < 0) | (number2 < 0)) {
+                throw new IllegalArgumentException("НЕ ВЕРНОЕ ВЫРАЖЕНИЕ");
+            } else {
+                result = calculated(number1, number2, operation);
+                System.out.println("_____Результат для римских цифр_____");
+
+                try {
+                    String resultRoman = convertNumToRoman(result);
+                    System.out.println(chast1 + " " + operation + " " + chast2 + " = " + resultRoman);
+                } catch (ArrayIndexOutOfBoundsException exception) {
+                    throw new IllegalArgumentException("В РИМСКОЙ СИСТЕМЕ НЕТ ОТРИЦАТЕЛЬНЫХ ЧИСЕЛ");
+                }
+            }
+        }
+    }
+    private static String convertNumToRoman(int numArabian) {
+        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };
+        final String x = roman[numArabian];
+        return x;
+    }
+    private static int romanToNumber(String roman) {
+        if (roman.equals("I")) {
+            return 1;
+        } else if (roman.equals("II")) {
+            return 2;
+        } else if (roman.equals("III")) {
+            return 3;
+        } else if (roman.equals("IV")) {
+            return 4;
+        } else if (roman.equals("V")) {
+            return 5;
+        } else if (roman.equals("VI")) {
+            return 6;
+        } else if (roman.equals("VII")) {
+            return 7;
+        } else if (roman.equals("VIII")) {
+            return 8;
+        } else if (roman.equals("IX")) {
+            return 9;
+        } else if (roman.equals("X")) {
+            return 10;
+        }
+        return -1;
+    }
+    public static int calculated(int num1, int num2, char operatorCalculated) {
+        int result = 0;
+        switch (operatorCalculated) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+        }
+        return result;
+    }
+}
+
